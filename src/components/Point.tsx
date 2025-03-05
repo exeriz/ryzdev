e3d5a95a3978dd5de32850a1a295e3bbf608c437
R <rizkyramadhanpd@gmail.com>
2025-03-05 21:39:38 +0700

    refactor: using svg component on lines

--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,11 +1,13 @@
 import { useId } from "react";
+import { Svg } from "@/components/Optimizing/Svg";
 
 export function Point() {
   const id = useId();
 
   return (
     <div className="pointer-events-none absolute inset-0 z-50 overflow-hidden lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] lg:overflow-visible">
-      <svg
+      <Svg
+        variant="custom"
         className="absolute left-[max(0px,calc(50%-18.6rem))] top-0 h-full w-1.5 lg:left-full lg:ml-1 xl:left-auto xl:right-1 xl:ml-0"
         aria-hidden="true"
       >
@@ -19,7 +21,7 @@ export function Point() {
           </pattern>
         </defs>
         <rect width="100%" height="100%" fill={`url(#${id})`} />
-      </svg>
+      </Svg>
     </div>
   );
 }
