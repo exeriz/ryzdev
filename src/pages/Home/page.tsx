efae746908e0263362ea6f5142dbfa5cd95c22c1
R <rizkyramadhanpd@gmail.com>
2025-04-01 19:17:14 +0700

    feat: add version check

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -138,15 +138,17 @@ export default function Home() {
                   />
                   {project.license !== null ? project.license : "No license"}
                 </Badge>
-                <Badge title="Current project version">
-                  <Svg
-                    variant="outline"
-                    width={12}
-                    height={12}
-                    draw={["M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"]}
-                  />
-                  {project.features?.[0]?.version}
-                </Badge>
+                {project.features?.[0]?.version && (
+                  <Badge title="Current project version">
+                    <Svg
+                      variant="outline"
+                      width={12}
+                      height={12}
+                      draw={["M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"]}
+                    />
+                    {project.features?.[0]?.version}
+                  </Badge>
+                )}
               </div>
             </section>
 
