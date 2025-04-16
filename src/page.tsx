5b5c4bbdb7e29a7e965d46519c020cd50db1a558
R <rizkyramadhanpd@gmail.com>
2025-04-16 22:06:31 +0700

    feat: delete page and route to changelog

--- src/page.tsx
+++ src/page.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router";
 import Home from "@/pages/Home/page";
-import Changelog from "@/pages/changelog/page";
 import About from "@/pages/introduction/page";
 import NotFound from "@/not-found";
 
@@ -9,7 +8,6 @@ export default function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="/introduction" element={<About />} />
-      <Route path="/changelog" element={<Changelog />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
