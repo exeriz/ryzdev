e7e0795015cd9756704c362818f03a133f3ceb22
R <rizkyramadhanpd@gmail.com>
2025-04-16 21:05:03 +0700

    feat: remove scroll up button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,18 +2,16 @@ import { ReactNode } from "react";
 import { Sidebar } from "@/components/Layout/Sidebar";
 import { Point } from "@/components/Point";
 import { Header } from "@/components/Layout/Header";
-import { ScrollButton } from "@/components/Buttons/ScrollButton";
 
 export function Layout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <>
+      <Header />
       <Sidebar />
       <div className="relative flex-auto">
         <Point />
         <div className="py-20 sm:py-32">{children}</div>
       </div>
-      <Header />
-      <ScrollButton />
     </>
   );
 }
