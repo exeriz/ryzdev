05a4d05a461869a863fe9683c056ca52e5b91203
R <rizkyramadhanpd@gmail.com>
2025-04-30 20:57:58 +0700

    refactor: project definition type update

--- src/assets/types/project.ts
+++ src/assets/types/project.ts
@@ -1,3 +1,13 @@
+export type FeatureItem = {
+  version: string;
+  list: string[];
+};
+
+export type Contributors = {
+  image: string;
+  url: string;
+}
+
 export interface Project {
   id: number;
   name: string;
@@ -10,15 +20,9 @@ export interface Project {
   };
   category: string;
   tags: string[];
-  features: Array<{
-    version: string;
-    list: string[];
-  }> | null;
+  features: FeatureItem[] | null;
   homepage: string | null;
   git: string;
   license: string | null;
-  contributors: {
-    image: string;
-    url: string;
-  }[] | null;
+  contributors: Contributors[] | null;
 }
