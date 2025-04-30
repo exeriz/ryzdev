d3c2c9ed1b7ef50698c815c206fffe81b3488db5
R <rizkyramadhanpd@gmail.com>
2025-04-30 21:28:39 +0700

    feat: contributor layout component updates

--- src/components/Layout/Project/ProjectContributors.tsx
+++ src/components/Layout/Project/ProjectContributors.tsx
@@ -7,22 +7,43 @@ export function ProjectContributors({
 }: Readonly<{
   contributors?: Project["contributors"];
 }>) {
-  return (
-    <div className="space-y-4 mt-6">
-      <SectionTitle icon="contributors">Contributors</SectionTitle>
+  const maxVisible = 5;
 
-      <div className="flex flex-wrap items-center gap-2">
-        {contributors?.length ? (
-          contributors.map((contributor) => (
-            <Button key={contributor.image} href={contributor.url}>
-              <img
-                alt={contributor.image}
-                src={contributor.image}
-                className="inline-block size-7 rounded-full"
-              />
-            </Button>
-          ))
-        ) : (
+  const visibleContributors = contributors?.slice(0, maxVisible) || [];
+  const extraCount = (contributors?.length || 0) - maxVisible;
+  const showSeeAll = (contributors?.length || 0) > maxVisible;
+
+  return (
+    <div className="w-full space-y-4 mt-6">
+      <div className="flex justify-between items-center w-full">
+        <SectionTitle icon="contributors">Contributors</SectionTitle>
+
+        {showSeeAll && <Button variant="link">See all &rarr;</Button>}
+      </div>
+
+      <div className="flex -space-x-1 p-0.5 overflow-hidden">
+        {visibleContributors.map((contributor, index) => {
+          const isLastVisible = index === maxVisible - 1 && extraCount > 0;
+
+          return (
+            <div key={contributor.image} className="relative">
+              <Button variant="default" href={contributor.url}>
+                <img
+                  src={contributor.image}
+                  alt="Contributor's profile"
+                  className="inline-block size-7 rounded-full ring-2 ring-gray-50 dark:ring-gray-950"
+                />
+              </Button>
+
+              {isLastVisible && (
+                <div className="absolute inset-0 bg-gray-950/50 flex items-center justify-center rounded-full text-xs font-semibold text-gray-100">
+                  +{extraCount}
+                </div>
+              )}
+            </div>
+          );
+        })}
+        {!contributors?.length && (
           <p className="text-sm/6">No contributors</p>
         )}
       </div>
