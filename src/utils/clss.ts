bf9aa0c60e900059c19d87500b109cc060efaa68
R <rizkyramadhanpd@gmail.com>
2025-07-27 21:11:01 +0700

    chore: adjustment of utility classes and adding new dependencies

--- src/utils/clss.ts
+++ src/utils/clss.ts
@@ -1,3 +1,6 @@
-export function clss(...classes: (string | undefined | null)[]): string {
-  return classes.filter(Boolean).join(" ");
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function clss(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
