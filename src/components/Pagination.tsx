49e9a3692d13c7efe4db4b8f9468c00904fcd740
R <rizkyramadhanpd@gmail.com>
2025-07-27 13:06:34 +0700

    feat: pagination adjustment

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,6 +6,8 @@ export function Pagination({
   totalPages,
   onChangePage,
 }: Readonly<PaginationInterface>) {
+  const year: number = new Date().getFullYear();
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -28,26 +30,32 @@ export function Pagination({
   };
 
   return (
-    <nav
-      aria-label="Pagination"
-      className="border-t border-gray-50/10 pt-8"
-    >
-      <div className="grid grid-cols-2 w-full">
-        <div className="flex justify-start">
-          {currentPage > 1 && (
-            <Button onClick={handlePreviousPage} variant="ghost" aria-label={`Button to page ${currentPage - 1}`} className="px-2.5 py-1">
-              <span aria-hidden="true">&larr;</span> Previous
-            </Button>
-          )}
+    <>
+      <nav
+        aria-label="Pagination"
+        className="border-t border-gray-50/10 mt-16 pt-6"
+      >
+        <div className="grid grid-cols-2 w-full">
+          <div className="flex justify-start">
+            {currentPage > 1 && (
+              <Button onClick={handlePreviousPage} variant="ghost" aria-label={`Button to page ${currentPage - 1}`} className="px-2.5 py-1">
+                <span aria-hidden="true">&larr;</span> Previous
+              </Button>
+            )}
+          </div>
+          <div className="flex justify-end">
+            {currentPage < totalPages && (
+              <Button onClick={handleNextPage} variant="ghost" aria-label={`Button to page ${currentPage + 1}`} className="px-2.5 py-1">
+                Next <span aria-hidden="true">&rarr;</span>
+              </Button>
+            )}
+          </div>
         </div>
-        <div className="flex justify-end">
-          {currentPage < totalPages && (
-            <Button onClick={handleNextPage} variant="ghost" aria-label={`Button to page ${currentPage + 1}`} className="px-2.5 py-1">
-              Next <span aria-hidden="true">&rarr;</span>
-            </Button>
-          )}
-        </div>
-      </div>
-    </nav>
+      </nav>
+
+      <footer className="w-full mt-6 py-2">
+        <p className="text-xs text-gray-700 dark:text-gray-300">&copy; {year} RyzDev. All rights Reserved.</p>
+      </footer>
+    </>
   );
 }
