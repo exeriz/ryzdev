9f403828f4c07b42bf2d017c1ca7d818665f08d4
R <rizkyramadhanpd@gmail.com>
2025-07-28 11:49:17 +0700

    fix: folder naming

--- src/page.tsx
+++ src/page.tsx
@@ -2,7 +2,7 @@ import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router";
 import { LoadingSpinner } from "@/components/ui/Spinner";
 
-const Home = lazy(() => import("@/pages/home/page"));
+const Home = lazy(() => import("@/pages/Home/page"));
 const About = lazy(() => import("@/pages/introduction/page"));
 const NotFound = lazy(() => import("@/not-found"));
 
